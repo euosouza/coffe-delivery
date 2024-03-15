076daef55967104f6492cba186a5d6edc66df818
Victor Souza <euosouza@gmail.com>
2024-03-15 19:13:05 -0300

    feat: criando pagina home

---
+++ src/assets/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+
+import { HomeContainer } from "./styles";
+
+
+import { Header } from "../../../components/Header";
+
+export function Home() {
+  return(
+    <HomeContainer>
+      <Header />
+    </HomeContainer>
+  );
+}
+
